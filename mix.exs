fbd57601ac447bf9a4bdacfb46a63ba42e704ce0
Ross Wilson <ross.wilson@bbc.co.uk>
2021-04-27 13:34:20 +0100

    Upgraded Elixir and npm packages

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule TotpExample.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:pot, "~>0.11.0"}
+      {:pot, "~>1.0.1"}
     ]
   end
 
